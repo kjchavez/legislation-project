0572c41f3c44f4d2cffeb8ba4f33238bde974429
Kevin Chavez <kevin.j.chavez@gmail.com>
2017-04-01 11:09:54 -0700

    Added model, training files, hyperparams

--- uscode/model.py
+++ uscode/model.py
@@ -1,13 +1,6 @@
 import tensorflow as tf
 import reader
-
-data_path = "/home/kevin/projects/legislation-project/uscode/processed-data"
-
-BATCH_SIZE = 4
-UNROLL_LEN = 10
-
-train, valid, test, vocab = reader._raw_data(data_path)
-x, y = reader.example_producer(train, BATCH_SIZE, UNROLL_LEN)
+import math
 
 def model_fn(features, targets, mode, params):
     cell = tf.contrib.rnn.BasicLSTMCell(
@@ -18,9 +11,7 @@ def model_fn(features, targets, mode, params):
                                 dtype=tf.float32)
 
     inputs = tf.nn.embedding_lookup(embedding, features)
-    print "Inputs shape:", inputs.get_shape()
-    inputs = tf.unstack(inputs, num=params['num_steps'], axis=1)
-	# tf.contrib.rnn.static_rnn
+    inputs = tf.unstack(inputs, num=params['unroll_length'], axis=1)
 
     # Note: This always feeds the 'true' next item in the sequence as input
     # into the RNN. We may want to have a mode where we feed the predicted
@@ -33,47 +24,67 @@ def model_fn(features, targets, mode, params):
         "softmax_w", [params['embedding_dim'], params['vocab_size']], dtype=tf.float32)
     softmax_b = tf.get_variable("softmax_b", [params['vocab_size']], dtype=tf.float32)
     logits = tf.matmul(output, softmax_w) + softmax_b
-    logits = tf.reshape(logits, [params['batch_size'], params['num_steps'],
+    logits = tf.reshape(logits, [params['batch_size'], params['unroll_length'],
                         params['vocab_size']])
-    print logits.get_shape()
-
-    loss = tf.contrib.seq2seq.sequence_loss(logits, targets,
-            tf.ones_like(targets, dtype=tf.float32),
-            average_across_timesteps=True,
-            average_across_batch=True)
 
     predictions = tf.arg_max(logits, 2)
-    lr = params['learning_rate']
-    max_grad_norm = 5  # params['max_grad_norm']
-    tvars = tf.trainable_variables()
-    grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars),
-                                      max_grad_norm)
-    optimizer = tf.train.GradientDescentOptimizer(lr)
-    train_op = optimizer.apply_gradients(
-        zip(grads, tvars),
-        global_step=tf.contrib.framework.get_or_create_global_step())
+    if mode == tf.contrib.learn.ModeKeys.TRAIN:
+        loss = tf.contrib.seq2seq.sequence_loss(logits, targets,
+                tf.ones_like(targets, dtype=tf.float32),
+                average_across_timesteps=True,
+                average_across_batch=True)
+        lr = params['learning_rate']
+        max_grad_norm = 5  # params['max_grad_norm']
+        tvars = tf.trainable_variables()
+        grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars),
+                                          max_grad_norm)
+        optimizer = tf.train.GradientDescentOptimizer(lr)
+        train_op = optimizer.apply_gradients(
+            zip(grads, tvars),
+            global_step=tf.contrib.framework.get_or_create_global_step())
+
+    else:
+        loss = None
+        train_op = None
 
     # Return predictions/loss/train_op/eval_metric_ops
-	# ModelFnOps(mode, predictions, loss, train_op, eval_metric_ops)
     return tf.contrib.learn.ModelFnOps(mode, predictions, loss, train_op, {})
 
-z = model_fn(x, y, None, {'vocab_size' : vocab.size(), 'embedding_dim': 8,
-                          'batch_size': BATCH_SIZE, 'num_steps': UNROLL_LEN,
-                          'learning_rate': 0.0001})
-init_op = tf.global_variables_initializer()
 
-coord = tf.train.Coordinator()
-with tf.Session() as sess:
-    sess.run(init_op)
-    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    _x, _y, _z = sess.run([x, y, z.loss])
+def sanity_check():
+    print("============== RUNNING SANITY CHECK =====================")
+    data_path = "/home/kevin/projects/legislation-project/uscode/processed-data"
 
-coord.request_stop()
-coord.join(threads)
+    BATCH_SIZE = 4
+    UNROLL_LEN = 10
 
-print "X"
-print _x
-print "Y"
-print _y
-print "Z"
-print _z
+    train, valid, test, vocab = reader._raw_data(data_path)
+
+    params = {}
+    params['vocab_size'] = vocab.size()
+    params['embedding_dim'] = 8
+    params['batch_size'] = BATCH_SIZE
+    params['unroll_length'] = UNROLL_LEN
+    params['learning_rate'] = 0.0001
+
+    x, y = reader.example_producer(train, BATCH_SIZE, UNROLL_LEN)
+    z = model_fn(x, y, tf.contrib.learn.ModeKeys.TRAIN,
+                 {'vocab_size' : vocab.size(), 'embedding_dim': 8,
+                  'batch_size': BATCH_SIZE, 'unroll_length': UNROLL_LEN,
+                  'learning_rate': 0.0001})
+    init_op = tf.global_variables_initializer()
+
+    coord = tf.train.Coordinator()
+    with tf.Session() as sess:
+        sess.run(init_op)
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        _x, _y, _z = sess.run([x, y, z.loss])
+
+    coord.request_stop()
+    coord.join(threads)
+
+    print "Average loss for rand init model: ", _z
+    print "Should be around: ", math.log(params['vocab_size'])
+
+if __name__ == "__main__":
+    sanity_check()
---
+++ uscode/train.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+import reader
+from model import model_fn
+import argparse
+import os
+import shutil
+import yaml
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_dir", default="/tmp/legislation-model")
+    parser.add_argument("--reset", action='store_true',
+                        help="If true, clears any data in 'model_dir' before "
+                             "training")
+    parser.add_argument("--data_path",
+        default="/home/kevin/projects/legislation-project/uscode/processed-data")
+    parser.add_argument("--train_steps", type=int, default=None)
+
+    # Hyperparameters can be set individually via commandline args...
+    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--unroll_steps", type=int, default=10)
+    parser.add_argument("--embedding_dim", type=int, default=8)
+    parser.add_argument("--learning_rate", type=float, default=1e-4)
+
+    # ...or with a YAML file containing all the values.
+    parser.add_argument("--hyperparams", default=None,
+                        help="yaml file of hyperparameters")
+
+    return parser.parse_args()
+
+args = parse_args()
+
+if args.hyperparams:
+    with open(args.hyperparams) as fp:
+        params = yaml.load(fp)
+
+else:
+    params = {}
+    params['embedding_dim'] = args.embedding_dim
+    params['batch_size'] = args.batch_size
+    params['unroll_length'] = args.unroll_steps
+    params['learning_rate'] = args.learning_rate
+
+train, valid, test, vocab = reader._raw_data(args.data_path)
+
+# Actual vocab size is determined after loading data 
+params['vocab_size'] = vocab.size()
+
+config = tf.contrib.learn.RunConfig(
+            log_device_placement=False,
+            keep_checkpoint_every_n_hours=1,
+            save_checkpoints_secs=60,
+            # save_checkpoints_steps=1000,
+            keep_checkpoint_max=5,
+            gpu_memory_fraction=0.7,
+            tf_random_seed=0,
+        )
+
+if args.reset and os.path.exists(args.model_dir):
+	shutil.rmtree(args.model_dir)
+
+estimator = tf.contrib.learn.Estimator(
+                model_fn=model_fn,
+                model_dir=args.model_dir,
+                config=config,
+                params=params)
+
+def train_input_fn():
+    return reader.example_producer(train, params['batch_size'],
+                                   params['unroll_length'])
+
+estimator.fit(input_fn=train_input_fn, steps=args.train_steps)
