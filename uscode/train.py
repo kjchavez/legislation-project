76860e4869994f4252a62a07bd5d794cd97484cf
Kevin Chavez <kevin.j.chavez@gmail.com>
2017-05-21 10:19:59 -0700

    more file renaming

--- uscode/train.py
+++ uscode/train.py
@@ -1,38 +1,36 @@
-import tensorflow as tf
-from tensorflow.python import debug as tf_debug
-import reader
-from model import LanguageModel
-import argparse
-import datetime
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import os
 import shutil
+import tensorflow as tf
 import yaml
+import datetime
+import argparse
 
+from uscode.model import InputData, LanguageModel, run_epoch
+import uscode.reader as reader
+
+ModeKeys = tf.contrib.learn.ModeKeys
 tf.logging.set_verbosity(tf.logging.INFO)
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_dir", default="/tmp/legislation-model")
+    parser.add_argument('--hparams', help='YAML file of hyperparameters')
+    parser.add_argument('--data_path', help="Where data files are stored.",
+                        default="/home/kevin/data/house-introduced-114")
+    parser.add_argument('--model_dir', help="to save model snapshots",
+                        default="snapshots/most_recent")
     parser.add_argument("--reset", action='store_true',
-                        help="If true, clears any data in 'model_dir' before "
-                             "training")
-    parser.add_argument("--data_path",
-        default="/home/kevin/projects/legislation-project/output")
-    parser.add_argument("--train_steps", type=int, default=None)
-    parser.add_argument("--debug", action='store_true')
-
-    # Hyperparameters can be set individually via commandline args...
-    parser.add_argument("--batch_size", type=int, default=4)
-    parser.add_argument("--unroll_steps", type=int, default=10)
-    parser.add_argument("--embedding_dim", type=int, default=8)
-    parser.add_argument("--num_layers", type=int, default=2)
-    parser.add_argument("--learning_rate", type=float, default=1e-4)
-
-    # ...or with a YAML file containing all the values.
-    parser.add_argument("--hyperparams", default=None,
-                        help="yaml file of hyperparameters")
-
+                        help="If true, clears any data in 'model_dir' ")
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--use_fp16', action='store_true')
     return parser.parse_args()
+
+def get_params(hparams_file):
+    with open(hparams_file) as fp:
+        return yaml.load(fp)
 
 def save_params(params, model_dir):
     if not os.path.exists(model_dir):
@@ -41,64 +39,73 @@ def save_params(params, model_dir):
     with open(os.path.join(model_dir, filename), 'w') as fp:
         yaml.dump(params, fp, default_flow_style=False)
 
-args = parse_args()
+def main():
+  args = parse_args()
+  if not args.data_path:
+    raise ValueError("Must set --data_path to data directory")
 
-if args.reset and os.path.exists(args.model_dir):
+  if args.reset and os.path.exists(args.model_dir):
 	shutil.rmtree(args.model_dir)
 
-if args.hyperparams:
-    with open(args.hyperparams) as fp:
-        params = yaml.load(fp)
-else:
-    params = {}
-    params['embedding_dim'] = args.embedding_dim
-    params['batch_size'] = args.batch_size
-    params['unroll_length'] = args.unroll_steps
-    params['learning_rate'] = args.learning_rate
+  raw_data = reader._raw_data(args.data_path)
+  train_data, valid_data, test_data, vocab_data = raw_data
 
-required = ('embedding_dim', 'batch_size', 'unroll_length', 'opt_method',
-        'opt_params', 'num_layers')
-if not all(param in params for param in required):
-    raise ValueError("Required parameters are missing.")
-    sys.exit(1)
+  params = get_params(args.hparams)
+  # TODO(kjchavez): A nice way for the model to declare required hyper params.
 
-train, valid, test, vocab = reader._raw_data(args.data_path)
+  save_params(params, args.model_dir)
 
-# Actual vocab size is determined after loading data 
-params['vocab_size'] = vocab.size()
+  eval_params = get_params(args.hparams)
+  eval_params['batch_size'] = 1
+  eval_params['num_steps'] = 1
 
-# Save params to model directory for future reference.
-save_params(params, args.model_dir)
+  with tf.Graph().as_default():
 
-config = tf.contrib.learn.RunConfig(
-            log_device_placement=False,
-            keep_checkpoint_every_n_hours=1,
-            save_checkpoints_secs=60,
-            # save_checkpoints_steps=1000,
-            keep_checkpoint_max=5,
-            gpu_memory_fraction=0.7,
-            tf_random_seed=0,
-        )
+    # TODO(kjchavez): This should move into the model?
+    initializer = tf.random_uniform_initializer(-params['init_scale'],
+                                                params['init_scale'])
+
+    with tf.name_scope("Train"):
+      train_input = InputData(params=params, data=train_data, name="TrainInput")
+      with tf.variable_scope("Model", reuse=None, initializer=initializer):
+        m = LanguageModel(mode=ModeKeys.TRAIN, params=params,
+                features=train_input.input_data, targets=train_input.targets,
+                epoch_size=train_input.epoch_size)
+      tf.summary.scalar("LearningRate", m.lr)
+
+    with tf.name_scope("Valid"):
+      valid_input = InputData(params=params, data=valid_data, name="ValidInput")
+      with tf.variable_scope("Model", reuse=True, initializer=initializer):
+        mvalid = LanguageModel(mode=ModeKeys.EVAL, params=params,
+                features=valid_input.input_data, targets=valid_input.targets,
+                epoch_size=valid_input.epoch_size)
+
+    with tf.name_scope("Test"):
+      test_input = InputData(params=eval_params, data=test_data, name="TestInput")
+      with tf.variable_scope("Model", reuse=True, initializer=initializer):
+        mtest = LanguageModel(mode=ModeKeys.EVAL, params=eval_params,
+                         features=test_input.input_data,
+                         targets=test_input.targets,
+                         epoch_size=test_input.epoch_size)
+
+    sv = tf.train.Supervisor(logdir=args.model_dir, save_summaries_secs=10,
+            save_model_secs=10)
+    with sv.managed_session() as session:
+      for i in range(params['max_max_epoch']):
+        print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
+        train_perplexity = run_epoch(session, m, eval_op=m.train_op,
+                                     verbose=True)
+        print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
+        valid_perplexity = run_epoch(session, mvalid)
+        print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
+
+      test_perplexity = run_epoch(session, mtest)
+      print("Test Perplexity: %.3f" % test_perplexity)
+
+      if args.model_dir:
+        print("Saving model to %s." % args.model_dir)
+        sv.saver.save(session, args.model_dir, global_step=sv.global_step)
 
 
-model = LanguageModel(params)
-estimator = tf.contrib.learn.Estimator(
-                model_fn=model.model_fn,
-                model_dir=args.model_dir,
-                config=config,
-                params=params)
-
-def train_input_fn():
-    return reader.example_producer(train, params['batch_size'],
-                                   params['unroll_length'])
-
-
-hooks = []
-if args.debug:
-    debug_hook = tf_debug.LocalCLIDebugHook()
-    debug_hook.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
-    hooks.append(debug_hook)
-
-estimator.fit(input_fn=train_input_fn,
-              steps=args.train_steps,
-              monitors=hooks)
+if __name__ == "__main__":
+    main()
