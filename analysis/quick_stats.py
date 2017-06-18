2f97e52765fa3c93f071cb6a8a8149016ec959a9
Kevin Chavez <kevin.j.chavez@gmail.com>
2017-06-18 10:02:02 -0700

    use seaborn style plots

--- analysis/quick_stats.py
+++ analysis/quick_stats.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(color_codes=True)
 
 def parse_args():
     parser = argparse.ArgumentParser()
