644c49030008f71d85d2054dd173b1968c20677e
Kevin Chavez <kevin.j.chavez@gmail.com>
2017-06-25 15:27:11 -0700

    get sample from cloud ml engine endpoint

--- site/server.py
+++ site/server.py
@@ -4,6 +4,7 @@ from flask import render_template
 from flask import request
 
 from grpc.beta import implementations
+import cloudml_client
 import numpy as np
 
 import tensorflow as tf
@@ -34,6 +35,12 @@ def _format_for_html(tokens):
     text.replace('\r\n',r).replace('\n\r',r).replace('\r',r).replace('\n',r)
     return text
 
+def get_cloud_sample(temp):
+    results = cloudml_client.predict_json(
+                'us-legislation-data',
+                'language_model',
+                {'temp': temp})
+    return _format_for_html(results[0]['tokens'])
 
 def get_sample(temp):
     request = client.PredictRequest()
@@ -63,7 +70,7 @@ def generate():
     result = {}
     print "Params data:", request.args
     temp = float(request.args.get("temp", 1.0))
-    sample = get_sample(temp)
+    sample = get_cloud_sample(temp)
     # TODO(kjchavez): Need vocabulary!
     result['text'] = sample
     return jsonify(result)
